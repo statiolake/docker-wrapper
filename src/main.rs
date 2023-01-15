ffdc4a6f98c9553413c6e7004e130d8190c8cbca
statiolake <statiolake@gmail.com>
2023-01-15 17:16:31 +0900

    feat: use WSL docker

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use anyhow::{bail, ensure, Result};
+use anyhow::{ensure, Result};
 use std::{
     fs,
-    path::{Path, PathBuf},
+    path::PathBuf,
     process::{Command, Stdio},
 };
 
 const DISTRO_ROOTFS_URL: &str =
     "https://cloud-images.ubuntu.com/wsl/jammy/current/ubuntu-jammy-wsl-amd64-wsl.rootfs.tar.gz";
 const DISTRO_NAME: &str = "custom-docker-host";
-const CONTEXT_NAME: &str = "custom-linux-docker-host";
-const DOCKER_HOST: &str = "localhost:22432";
 
 fn home_dir() -> PathBuf {
     dirs::home_dir().unwrap_or_else(|| panic!("critical error: failed to get home directory"))
@@ -20,26 +18,24 @@ fn distro_dir_path(name: &str) -> PathBuf {
     home.join("wsl-distros").join(name)
 }
 
-fn find_native_docker_cli_path() -> PathBuf {
-    let mut path = home_dir();
-    path.extend(Path::new("scoop/shims/docker.exe").components());
-    path
-}
-
-fn output(args: &[&str], set_context: bool) -> Result<String> {
-    // eprintln!("output: {:?}", args);
+fn output(args: &[&str]) -> Result<String> {
+    eprintln!("output: {:?}", args);
     let mut cmd = Command::new(args[0]);
     cmd.args(&args[1..]);
-    if set_context {
-        cmd.env("DOCKER_CONTEXT", CONTEXT_NAME);
-    }
     let output = cmd.output()?;
+    ensure!(output.status.success(), "command failed");
 
     Ok(String::from_utf8_lossy(&output.stdout).to_string())
 }
 
-fn run(args: &[&str], silent: bool, set_context: bool) -> Result<bool> {
-    // eprintln!("run: {:?}", args);
+fn output_in_wsl(args_in_wsl: &[&str]) -> Result<String> {
+    let mut args = vec!["wsl", "-d", DISTRO_NAME, "-e"];
+    args.extend(args_in_wsl);
+    output(&args)
+}
+
+fn run(args: &[&str], silent: bool) -> Result<bool> {
+    eprintln!("run: {:?}", args);
     let (stdout, stderr) = if silent {
         (Stdio::null(), Stdio::null())
     } else {
@@ -48,10 +44,6 @@ fn run(args: &[&str], silent: bool, set_context: bool) -> Result<bool> {
 
     let mut cmd = Command::new(args[0]);
     cmd.args(&args[1..]).stdout(stdout).stderr(stderr);
-    if set_context {
-        cmd.env("DOCKER_CONTEXT", CONTEXT_NAME);
-    }
-
     let status = cmd.spawn()?.wait()?;
 
     Ok(status.success())
@@ -60,41 +52,7 @@ fn run(args: &[&str], silent: bool, set_context: bool) -> Result<bool> {
 fn run_in_wsl(args_in_wsl: &[&str], silent: bool) -> Result<bool> {
     let mut args = vec!["wsl", "-d", DISTRO_NAME, "-e"];
     args.extend(args_in_wsl);
-    run(&args, silent, false)
-}
-
-fn ensure_native_docker_cli() -> Result<()> {
-    let native_cli = find_native_docker_cli_path();
-    if !native_cli.exists() {
-        bail!(
-            "failed to find native docker cli at {}",
-            native_cli.display()
-        );
-    }
-
-    let native_cli = native_cli.to_string_lossy();
-
-    if output(&[&*native_cli, "context", "ls"], false)?.contains(CONTEXT_NAME) {
-        return Ok(());
-    }
-
-    ensure!(
-        run(
-            &[
-                &*native_cli,
-                "context",
-                "create",
-                CONTEXT_NAME,
-                "--docker",
-                &format!("host=tcp://{DOCKER_HOST}")
-            ],
-            true,
-            false,
-        )?,
-        "failed to create docker context"
-    );
-
-    Ok(())
+    run(&args, silent)
 }
 
 fn ensure_docker() -> Result<()> {
@@ -135,7 +93,6 @@ fn download_and_import_rootfs() -> Result<()> {
                     &download_path.display().to_string(),
                 ],
                 false,
-                false
             )?,
             "failed to download rootfs"
         );
@@ -151,7 +108,6 @@ fn download_and_import_rootfs() -> Result<()> {
                 &download_path.display().to_string()
             ],
             false,
-            false
         )?,
         "failed to import distro"
     );
@@ -185,9 +141,7 @@ fn setup_docker_on_distro() -> Result<()> {
             &[
                 "sh",
                 "-c",
-                &format!(
-                    r#"mkdir -p /etc/docker && echo '{{"features":{{"buildkit":true}},"hosts":["{DOCKER_HOST}"]}}' > /etc/docker/daemon.json"#
-                )
+                r#"mkdir -p /etc/docker && echo '{"features":{"buildkit":true}}' > /etc/docker/daemon.json"#
             ],
             true
         )?,
@@ -198,11 +152,7 @@ fn setup_docker_on_distro() -> Result<()> {
 }
 
 fn convert_path(from: &str) -> Result<String> {
-    output(
-        &["wsl", "-d", DISTRO_NAME, "-e", "wslpath", "-u", from],
-        false,
-    )
-    .map(|s| s.trim().to_string())
+    output_in_wsl(&["wslpath", "-u", from]).map(|s| s.trim().to_string())
 }
 
 fn modify_args(args: &mut [String]) -> Result<()> {
@@ -212,6 +162,12 @@ fn modify_args(args: &mut [String]) -> Result<()> {
 
     if args[0] == "create" {
         fix_bind_mount_path(args)?;
+    }
+
+    if args[0] != "exec" {
+        for arg in args {
+            fix_arg_containing_backslash(arg)?;
+        }
     }
 
     Ok(())
@@ -243,18 +199,52 @@ fn fix_bind_mount_path(args: &mut [String]) -> Result<()> {
     Ok(())
 }
 
-fn main() -> Result<()> {
-    ensure_native_docker_cli()?;
-    ensure_docker()?;
-
-    let mut args: Vec<_> = std::env::args().skip(1).collect();
-    modify_args(&mut args)?;
-
-    let native_cli_path = find_native_docker_cli_path();
-    let native_cli_path = native_cli_path.to_string_lossy();
-    let mut native_args = vec![&*native_cli_path];
-    native_args.extend(args.iter().map(|arg| &**arg));
-    ensure!(run(&native_args, false, true)?, "docker failed");
+fn fix_arg_containing_backslash(arg: &mut String) -> Result<()> {
+    if arg.contains('\\') {
+        if let Ok(path) = convert_path(arg) {
+            *arg = path;
+        }
+    }
 
     Ok(())
+}
+
+fn execute_wrapped(args: &mut [String]) -> Result<()> {
+    ensure_docker()?;
+    modify_args(args)?;
+    let mut native_args = vec!["docker"];
+    native_args.extend(args.iter().map(|arg| &**arg));
+    ensure!(run_in_wsl(&native_args, false)?, "docker failed");
+    Ok(())
+}
+
+fn handle_extra_subcommand(args: &mut [String]) -> Result<bool> {
+    if args.is_empty() {
+        return Ok(false);
+    }
+
+    match &*args[0] {
+        "stop" => {
+            run(&["wsl", "--shutdown"], true)?;
+
+            Ok(true)
+        }
+        "reset-registration" => {
+            run(&["wsl", "--shutdown"], true)?;
+            run(&["wsl", "--unregister", DISTRO_NAME], true)?;
+            ensure_docker()?;
+
+            Ok(true)
+        }
+        _ => Ok(false),
+    }
+}
+
+fn main() -> Result<()> {
+    let mut args: Vec<_> = std::env::args().skip(1).collect();
+    if handle_extra_subcommand(&mut args)? {
+        return Ok(());
+    }
+
+    execute_wrapped(&mut args)
 }
